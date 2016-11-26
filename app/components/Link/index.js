6799b9c5aaf8ba28662bed56467b6271d85b660e
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2016-11-26 10:15:01 +0800

    adding style loader for css modules

--- app/components/Link/index.js
+++ app/components/Link/index.js
@@ -1,9 +1,12 @@
 import React, {PropTypes} from 'react';
+
+import styles from './style.css'
+
 export default class Avatar extends React.Component {
   render() {
     return (
       <div className="avatar">
-        <h2 className="neco">me too</h2>
+        <h2 className={styles.me}>me too</h2>
       </div>
     );
   }
